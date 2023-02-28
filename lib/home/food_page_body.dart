0711a17aef93e550f2b77fbbb1a622bdf0c38021
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-28 08:55:52 +0300

    :heavy_plus_sign::art: Dots indicator added

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
@@ -34,14 +35,27 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 320,
-      child: PageView.builder(
-          controller: pageController,
-          itemCount: 5,
-          itemBuilder: (context, position){
-        return _buildPageItem(position);
-      })
+    return Column(
+      children: [
+        SizedBox(
+          height: 320,
+          child: PageView.builder(
+              controller: pageController,
+              itemCount: 5,
+              itemBuilder: (context, position){
+            return _buildPageItem(position);
+          })
+        ),
+          DotsIndicator(
+          dotsCount: 5,
+          position: _currPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          ),)
+      ],
     );
   }
   Widget _buildPageItem(int index){
