1d7e0be089eb235885e305376de1efa9e964b0ce
Calvin Ngugi <ncalvin67@gmail.com>
2023-02-28 08:14:17 +0300

    :lipstick: Adding shadows

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
@@ -16,7 +15,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
   var _currPageValue = 0.0;
   double _scaleFactor = 0.8;
-  double height = 220;
+  double _height = 220;
 
   @override
   void initState(){
@@ -35,7 +34,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -46,21 +45,24 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-    Matrix4 matrix = new Matrix4.identity();
+    Matrix4 matrix = Matrix4.identity();
     if (index == _currPageValue.floor()){
       var currScale = 1-(_currPageValue-index)*(1-_scaleFactor);
-      var currTrans =  height*(1-currScale)/2;
+      var currTrans =  _height*(1-currScale)/2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
     }else if(index == _currPageValue.floor()+1){
-      var currScale = _scaleFactor+(_currPageValue-index+1)*(1-_scaleFactor);
-      var currTrans =  height*(1-currScale)/2;
+      var currScale = _scaleFactor+(_currPageValue - index + 1)*(1 - _scaleFactor);
+      var currTrans =  _height*(1-currScale)/2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
     }else if(index == _currPageValue.floor()-1) {
       var currScale = _scaleFactor +
           (_currPageValue - index - 1) * (1 + _scaleFactor);
-      var currTrans = height * (1 - currScale) / 2;
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1)
         ..setTranslationRaw(0, currTrans, 0);
+    }else{
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 0);
     }
 
     return Transform(
@@ -71,7 +73,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             height: 220,
             margin: const EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(20),
               image: const DecorationImage(
                 fit: BoxFit.cover,
                 image: AssetImage("assets/images/food1.jpg")
@@ -82,32 +84,47 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             alignment: Alignment.bottomCenter,
             child: Container(
               height: 120,
-              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              margin: const EdgeInsets.only(left: 18, right: 18, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 6)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-2, 0)
+                  ),
+                  BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(2, 0)
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(20),
                 color: Colors.white
               ),
               child: Container(
-                padding: EdgeInsets.only(top: 5.0, left: 15, right: 20),
+                padding: const EdgeInsets.only(top: 5.0, left: 15, right: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      padding: EdgeInsets.only(left: 8, top: 10),
+                      padding: const EdgeInsets.only(left: 8, top: 10),
                         child: BigText(text: 'Beef burger and fries')),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
                     Row(
                       children: [
                         Wrap(
                           children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor,)),
                         ),
-                        SizedBox(width: 10),
+                        const SizedBox(width: 5),
                         SmallText(text: '4.5'),
-                        SizedBox(width: 10),
+                        const SizedBox(width: 5),
                         SmallText(text: '1287 comments'),
                       ],
                     ),
-                    SizedBox(height: 15),
+                    const SizedBox(height: 15),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
